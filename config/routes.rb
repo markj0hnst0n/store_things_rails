57337fd1e8e93f89f0c0b17d2025ca239ff06b46
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-18 20:51:32 +0100

    routing fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'units#index'
   get 'admin/units/new', to: 'units#new'
   post 'admin/units/create', to: 'units#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
